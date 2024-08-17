b7458158bbca924936e88619b1b9481d3c2aef9c
Magdy Abas <132621536+magdy-abas@users.noreply.github.com>
2024-08-18 00:19:13 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBlankComponent } from './components/nav-blank/nav-blank.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBlankComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'e-commerce';
