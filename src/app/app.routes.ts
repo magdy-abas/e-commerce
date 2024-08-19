4b350f8b4addf15b46a4a424d2ec09838f5ed71e
Magdy Abas <132621536+magdy-abas@users.noreply.github.com>
2024-08-19 06:11:49 +0300

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,44 @@
 import { Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { BlankLayoutComponent } from './layouts/blank-layout/blank-layout.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CartComponent } from './components/cart/cart.component';
+import { BrandsComponent } from './components/brands/brands.component';
+import { CategoriesComponent } from './components/categories/categories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    //path:'auth'
+    path: '',
+    component: AuthLayoutComponent,
+
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent, title: 'Login' },
+      { path: 'register', component: RegisterComponent, title: 'Register' },
+    ],
+  },
+  {
+    //path:'blank'
+    path: '',
+    component: BlankLayoutComponent,
+
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: HomeComponent, title: 'Home' },
+      { path: 'products', component: ProductsComponent, title: 'Products' },
+      { path: 'cart', component: CartComponent, title: 'Cart' },
+      { path: 'brands', component: BrandsComponent, title: 'Brands' },
+      {
+        path: 'categories',
+        component: CategoriesComponent,
+        title: 'Categories',
+      },
+    ],
+  },
+  { path: '**', component: NotFoundComponent, title: 'Not Found' },
+];
--- src/app/components/nav-blank/nav-blank.component.ts
+++ src/app/components/nav-blank/nav-blank.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-blank',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './nav-blank.component.html',
-  styleUrl: './nav-blank.component.scss'
+  styleUrl: './nav-blank.component.scss',
 })
-export class NavBlankComponent {
-
-}
+export class NavBlankComponent {}
